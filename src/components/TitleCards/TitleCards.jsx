b3bc26c30c2c3c8e510af115287578ba35726d03
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-06-17 23:07:17 +0530

    Mapped title cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data.js'
 
 const TitleCards = () => {
   return (
-    <div className='titlecards'></div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+      {cards_data.map((card, index) => {
+      return <div className="card" key={index}>
+        <img src={card.image} alt="" />
+        <p>{card.name}</p>
+      </div>
+})}
+      </div>
+    </div>
   )
 }
 
