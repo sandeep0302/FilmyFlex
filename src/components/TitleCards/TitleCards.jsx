49f5518850dbb3dcf16cb19804853f5681da3017
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-06-21 18:44:15 +0530

    TMDB Api fetch

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data.js'
 
@@ -6,7 +6,19 @@ import cards_data from '../../assets/cards/Cards_data.js'
 
 const TitleCards = ({title,category}) => {
 
+  const [apiData,setApiData] =useState([]);
+
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMTY3YmY4YjhjNDc0OGZkNjc0ZjNiODcxYzY3OGMxNCIsInN1YiI6IjY2NzU3NWFlN2FkYmJhZjIzZjI4ZjQxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xbq6Cw3Yg2GFfJMjXghTEmC8bgcbAQ2YMeZ1pyVPgGM'
+    }
+  };
+  
+  
 
 const handleWheel = (event) => {
   event.preventDefault();
@@ -14,16 +26,22 @@ const handleWheel = (event) => {
 }
 
 useEffect(()=>{
+  fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
+
   cardsRef.current.addEventListener('wheel',handleWheel);
 },[])
   return (
     <div className='title-cards'>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-      {cards_data.map((card, index) => {
+      {apiData.map((card, index) => {
       return <div className="card" key={index}>
-        <img src={card.image} alt="" />
-        <p>{card.name}</p>
+        <img src={`https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt="" />
+        <p>{card.original_title}</p>
       </div>
 })}
       </div>
