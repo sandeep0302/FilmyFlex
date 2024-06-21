33e34b0b0162491db09798b6d87f0fc471c5727d
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-06-21 21:29:50 +0530

    player component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Home from './pages/Home/Home'
 import { Routes,Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player.jsx'
+
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route  path='/login' element={<Login />}/>
+        <Route  path='/player/:id' element={<Player/>}/>
       </Routes>
       
   
