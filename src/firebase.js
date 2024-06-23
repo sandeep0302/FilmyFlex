0fdcb28684b5d7e323390d4bd51209d3ae9c0467
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-06-23 13:23:45 +0530

    Added SignUp Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA4tGL_PWRlXipNsz_EAJk9wpoGqNEa3DU",
@@ -11,3 +13,39 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+const signup = async (name ,email, password) => {
+try {
+    const res =  await createUserWithEmailAndPassword(auth,email,password);
+    const user = res.user;
+    await addDoc(collection(db,"user"),{
+      uid:user.uid,
+      name,
+      authProvider:"local",
+      email,  
+    });
+} catch (error) {
+    console.log(error);
+    alert(error);
+}
+}
+
+const login = async(email,password) => {
+try {
+   await signInWithEmailAndPassword(auth,email,password);
+
+} catch (error) {
+    console.log(error);
+    alert(error);
+}
+}
+
+const logout =  () => {
+    signOut(auth);
+}
+
+export {auth, db, login, signup, logout};
