f2083f93e88b345a1e210f32e13ea26226c87b7b
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-06-23 11:24:43 +0530

    Initializing firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4tGL_PWRlXipNsz_EAJk9wpoGqNEa3DU",
+  authDomain: "netflix-clone-d16eb.firebaseapp.com",
+  projectId: "netflix-clone-d16eb",
+  storageBucket: "netflix-clone-d16eb.appspot.com",
+  messagingSenderId: "174965074643",
+  appId: "1:174965074643:web:3b99235441159285a48226"
+};
+
+
+const app = initializeApp(firebaseConfig);
